c6da439c7395305b0815d96f268356c6ba018e5b
Hawgnes <randomsergeant@hotmail.com>
2017-10-31 16:02:29 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/lab21loancalculator/ResultActivity.java
@@ -0,0 +1,27 @@
+package my.edu.tarc.lab21loancalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        // TODO : Link UI to program
+        // get input data from another activity
+        Intent intent = getIntent();
+        String status = intent.getStringExtra(MainActivity.LOAN_STATUS);
+        double payment = intent.getDoubleExtra(MainActivity.LOAN_PAYMENT, 0);
+
+        // TODO : display outputs here
+    }
+
+    public void closeActivity(View view){
+        finish();
+    }
+}
